23548572fd0cba562b283aed1e743b5e81333b47
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-15 23:21:06 +0530

    nav registered

--- Proj1/src/App.jsx
+++ Proj1/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
-    
+      <Navigation />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
