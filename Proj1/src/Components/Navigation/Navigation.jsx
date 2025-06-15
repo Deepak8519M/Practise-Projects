90febd56e9f654de6d931fb06783314d8d48c708
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-15 23:27:25 +0530

    code added

--- Proj1/src/Components/Navigation/Navigation.jsx
+++ Proj1/src/Components/Navigation/Navigation.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Navigation.css";
 
 function Navigation() {
-  return <div>hello</div>;
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="http://localhost:5173/images/logo.png" alt="" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
