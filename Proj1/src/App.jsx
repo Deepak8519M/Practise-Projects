9e6e92af2ed112580f083dd824cc7f3103583980
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-15 23:36:14 +0530

    HeroTop Added

--- Proj1/src/App.jsx
+++ Proj1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import "./App.css";
+import HeroTop from "./Components/HeroTop/HeroTop";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <HeroTop />
     </div>
   );
 }
